fa91d1dea810e5c5b625365799e3d5d8e52fde5d
Sumit <red.fyrd@gmail.com>
2017-11-24 22:43:46 +0000

    refoctoring app.js

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+  //=========================================================================================================================                                    
+ //  DESIGNED AND DEVELOPED BY "SUMIT ARYA" : "<ARYASUMIT145@GMAIL.COM>" "WWW.CANATECH.IN" "WWW.SUMITARYA.TK"
+//=========================================================================================================================                                    
+
+
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
@@ -7,42 +12,41 @@ var ObjectId = require('mongodb').ObjectId;
 var  path = require('path');
 var port = process.env.PORT || 3000;
 
-// var posRoutes=require("./routes/posRoutes")
 
-// var billRoutes=require("./routes/billRoutes")
-// app.use('/', posRoutes);
-// app.use('/bill', billRoutes)
-
+  //=========================================================================================================================                                    
+ //  CONNECT AND USE THE REQUIRED  web application framework for Node.js
+//=========================================================================================================================                                    
 
 
     // mongoose.connect("mongodb://localhost/cafe_o2")
     mongoose.connect("mongodb://sumitarya:behaPPYhaha99!@ds255715.mlab.com:55715/o2db")
     mongoose.Promise = require('bluebird');
     app.use(express.static(path.join(__dirname, 'public')));
-    // app.use("/public", express.static(__dirname + "/public"));
     app.use(express.static(__dirname + "/views"));
     app.use(bodyParser.json());
 
     app.use(bodyParser.urlencoded({extended:true}));
     app.set("view engine","ejs");
-    
-    //schema setup
-    
-    //Meal model==================
+
+
+  //=========================================================================================================================                                    
+ //  MEAL MODEL AND SCHEMA
+//=========================================================================================================================                                    
+
 
 var mealSchema = new mongoose.Schema({
     mealname: String,
     mealprice: String
-});
+    });
 
 var Meal = mongoose.model("Meal", mealSchema);
 
 
 
 
-
-    
-
+  //=========================================================================================================================                                    
+ //  BILL MODEL AND SCHEMA
+//=========================================================================================================================                                    
 
     
 var billSchema = new mongoose.Schema({
@@ -55,218 +59,99 @@ var billSchema = new mongoose.Schema({
             ref: "Meal"
         }
         ]
-});
+    });
 
 var Bill = mongoose.model("Bill", billSchema);
     
 
-// Bill.create({
-//     bn:3,
-//     waitername: "Sat", 
-//     tablenumber:"4"
-// }, function(err, bill) {
-//     if(err){
-//         console.log(err)
-//     }
-    
-    
-//     console.log(bill)
-    
-// })
-
-
-
-
-//   var newBill = new Bill({
-//      bn:3,
-//      waitername: "Dog"
-//  });
- 
-// //  newBill.numbs.push({
-// //      ordernumber:4
-// //  })
-//   newBill.save(function(err, bill){
-//     if(err){
-//         console.log(err)
-//     } console.log(bill)
-// }) 
-
-
-
-
-
-
+  //=========================================================================================================================                                    
+ //  ROOT ROUTE - GET - NEW BILL FORM
+//=========================================================================================================================                                    
 
  
 app.get("/",function(req, res){
     
-Bill.count(function(err, count){
-    if(err){
-        console.log(err)
-    }
-  console.log("Total matches: "+count);
-
-
-    
-//  Bill.findOne({ "$query":{}, "$orderby":{ "bn": -1 }}, function(err, bill){
-//      if(err){
-//          console.log(err)
-//      }console.log(bill) 
-//      var x=bill.bn
-//     var nx=bill.bn+1
-//      console.log("This: " + x)
-//      console.log("Next: " + nx)
-         
-         
-         
-         
-         
-//      })
-
-            res.render("landing")
+    Bill.count(function(err, count){
+        if(err){
+            console.log(err)
+        }
+        console.log("Total matches: "+count);
+        res.render("landing")
             ;});
          });
 
-// Meal.create({
-//             mealname:"paratha",
-//             mealprice: "20"
-    
-// },            function(err,meal){
-//                 Bill.findOne({billnumber:"A012"}, function(err, foundBill){
-//                     if(err){
-//                         console.log(err)
-//                     } else foundBill.meals.push(meal)
-//                       foundBill.save(function(err, data){
-//                           if(err){
-//                               console.log(err)
-//                           } else {
-//                               console.log(data)
-//                           }
-//                       })
-//                 })
-//             }
-//             )
-
-// Bill.findOne({billnumber:"A012"}).populate("meals").exec(function(err, bill){
-//     if(err){
-//         console.log(err)
-//     } console.log(bill)
-// })
 
 
-    
-    // ====================================================================================
-    // Meal.create(
-    //                 {   
-    //                 name: "Changezi",
-    //                 category:"Main course - Mughlai",
-    //                 image:"https://i.ndtvimg.com/i/2016-07/chicken-korma_625x350_71467713811.jpg",
-    //                 comment:"Spicy",
-    //                 price:"600"
-    //                 }, function(err, Meal){
-    //                     if(err){
-    //                         console.log(err)
-    //                     } else {
-    //                     console.log("meal created");
-    //                     console.log(Meal)
-    //                 }
-    //             });
-    
+  //=========================================================================================================================                                    
+ //  MEALS ROUTE - GET - DISPLAY ALL MEALS
+//=========================================================================================================================                                    
 
-    
-    // ==========================================================================================
-                        // var meals=  [
-                                    
-                        //              {   name: "spring roll",
-                        //                 category:"main course",
-                        //                 image:"http://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-2.jpg",
-                        //                 comment:"spicy",
-                        //                 price:"100"
-                        //             },{   name: "spring roll",
-                        //                 category:"main course",
-                        //                 image:"http://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-2.jpg",
-                        //                 comment:"spicy",
-                        //                 price:"100"
-                        //             },{   name: "spring roll",
-                        //                 category:"main course",
-                        //                 image:"http://www.vegrecipesofindia.com/wp-content/uploads/2015/10/spring-rolls-recipe-2.jpg",
-                        //                 comment:"spicy",
-                        //                 price:"100"
-                        //             }
-                        //             ]
-// =========================================================================================================================                                    
-        
-// =========================================================================================================================                                    
-        app.get("/meals", function(req, res){
+ app.get("/meals", function(req, res){
             
-                          //Get meals from db
-
-                          
-                        Meal.find({}, function(err, allMeals){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                res.render("meals",{meals:allMeals}); 
-                          
-                            }
-                        })
-        });
+                       
+        Meal.find({}, function(err, allMeals){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("meals",{meals:allMeals}); 
+          
+            }
+        })
+    });
         
    
-// =========================================================================================================================                                    
-        app.post("/meals", function(req, res){
-        
-                var name = req.body.name;
-                var category = req.body.category;
-                var image = req.body.image;
-                var comment = req.body.comment;
-                var price = req.body.price;
-      
-        var newMeal = {name:name, category:category, image:image, comment:comment, price:price}
-        
-        // meals.push(newMeal);
-        
-        Meal.create(newMeal, function(err, newlyCreated){
-            if(err){
+  //=========================================================================================================================                                    
+ //  MEALS ROUTE - POST - DISPLAY ALL MEALS
+//=========================================================================================================================                                    
+       
+app.post("/meals", function(req, res){
+
+        var name = req.body.name;
+        var category = req.body.category;
+        var image = req.body.image;
+        var comment = req.body.comment;
+        var price = req.body.price;
+
+var newMeal = {name:name, category:category, image:image, comment:comment, price:price}
+
+
+
+    Meal.create(newMeal, function(err, newlyCreated){
+        if(err){
                 console.log(err)
             } else {
-                res.redirect("/meals")
-            }
-        });
-       });
-// =========================================================================================================================                                    
+                    res.redirect("/meals")
+        }
+    });
+    });
+
+  //=========================================================================================================================                                    
+ //  ROOT ROUTE - POST - CREATE A NEW BILL WITH AUTO GENEREATED BILL NUMBER
+//=========================================================================================================================                                    
 
    
-                app.post("/", function(req, res){
+app.post("/", function(req, res){
         
         
         Bill.count(function(err, count){
             if(err){
                 console.log(err)
             }
-          console.log("Total matches: "+count);
-        
-                
-        
+            console.log("Total matches: "+count);
                         var bn = count+1;
                         var tablenumber = req.body.tablenumber;
                         var waitername = req.body.waitername;
        
-      console.log("New Match : " +bn)
+        console.log("New Match : " +bn)
         var newBill = new Bill({bn:bn, tablenumber:tablenumber, waitername: waitername})
         
         
-            // Bill.find().count(function(err, count){
-            //     console.log("Number of docs: ", count );
-            
-    
-        // meals.push(newMeal);
         
-         Bill.create(newBill, function(err, bill){
+        Bill.create(newBill, function(err, bill){
             if(err){
                 console.log(err);
             } else {
-                 //redirect back to campgrounds
+                 //redirect back
                  newBill.save(function(err, bill){
                     if(err){
                         console.log(err)
@@ -284,51 +169,31 @@ Bill.count(function(err, count){
     
         
         
+  //=========================================================================================================================                                    
+ //  BILLS ROUTE - GET - DISPLAY ALL BILLS
+//=========================================================================================================================                                    
+
         
-   
-        
-        // ======================================
-        
-      app.get("/bills", function(req, res) {
-          var bill = req.body._id;
-       
+app.get("/bills", function(req, res) {
           
-          Bill.find({}, function(err, allBills){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                res.render("bill",{bills:allBills}); 
-                         
-                            }
-                        })
-            
+        Bill.find({}, function(err, allBills){
+                    if(err){
+                        console.log(err);
+                    } else {
+                        res.render("bill",{bills:allBills}); 
+                 
+                    }
+                })
+    
       })
-// =========================================================================================================================                                    
 
-// SHOW - shows more info about one bill
 
-    // app.get("/bills/:id", function(req, res) {
-        
-    //     // find campground with provided id
-    //     Bill.findById(req.params.id).populate("bills").exec(function(err, foundBill){
-    //         if(err){
-    //             console.log(err)
-    //         } else {
-    //             // console.log(foundBill)
-    //             // render show template with that campground
-    //             // res.send(Bill)
-    //             // res.render("bill", {bill: foundBill});
-    //         }
-    //     });
-    //     req.params.id
-
-    // });
-//  ======================================================================================
-// SHOW PAGE FOR SPECIFIC BILL ID, DISPLAY MEALS IS THAT BILL, DISPLAY ALL MEALS
+  //  ======================================================================================
+ // SHOW PAGE FOR SPECIFIC BILL ID, DISPLAY MEALS IS THAT BILL, DISPLAY ALL MEALS
 //  ======================================================================================
    
 
-    app.get("/bills/:id", function(req, res) {
+app.get("/bills/:id", function(req, res) {
         
         
         Bill.findById(req.params.id).populate("meals bills").exec(function(err, foundBill){
@@ -351,21 +216,8 @@ Bill.count(function(err, count){
         });
     
  
-                //  Meal.find({}, function(err, allMeals){
-                //             if(err){
-                //                 console.log(err);
-                //             } else {
-                //                 res.render("bill",{meals:allMeals}); 
-                        
-                //             }
-                //         })
-
-                
-                
-                
-                
  // =========================================================================================================================                                    
-// new meal route
+//  GET A FORM TO ADD A NEW MEAL
 // =========================================================================================================================                                    
 
 
@@ -382,30 +234,31 @@ app.get("/bills/:id/meals/new", function(req, res) {
 
                 
 // =========================================================================================
-// add meal to specific bill
+// POST A NEW MEAL
 // =========================================================================================                
                 
 app.post("/bills/:id/meals", function(req, res) {
-    Bill.findById(req.params.id, function(err, bill) {
-        if(err){
-            console.log(err)
-        } 
-        
-        
-        Meal.create(req.body.meal, function(err, meal) {
+    
+        Bill.findById(req.params.id, function(err, bill) {
             if(err){
                 console.log(err)
-            }bill.meals.push(meal)
-           
-            bill.save()
-             console.log(bill)
-            res.redirect('/bills/' + bill._id);
+            } 
+            
+            
+            Meal.create(req.body.meal, function(err, meal) {
+                if(err){
+                    console.log(err)
+                }bill.meals.push(meal)
+               
+                bill.save()
+                 console.log(bill)
+                res.redirect('/bills/' + bill._id);
+            })
         })
-    })
 })
  
   // =========================================================================================================================                                    
-// create meal and push to specific bill
+ // create meal and push to specific bill
 // =========================================================================================================================                                    
 
 
@@ -426,7 +279,21 @@ app.post("/bills/:id/meals", function(req, res) {
 })
  
  
+ 
+ 
+ 
  // =========================================================================================================================                                    
  app.listen(port, process.env.IP, function(){
-        console.log("Server started! App is running on PORT : " + process.env.PORT);
+        console.log("Sumit's Server started! App is running on PORT : " + process.env.PORT);
     });
+    
+    
+    
+    
+    
+    
+    
+    
+  //=========================================================================================================================                                    
+ //  DESIGNED AND DEVELOPED BY "SUMIT ARYA" : "<ARYASUMIT145@GMAIL.COM>" "WWW.CANATECH.IN" "WWW.SUMITARYA.TK"
+//=========================================================================================================================                                    
